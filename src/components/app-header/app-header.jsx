74e31224425e1ad1951d29f701f3403bfb4690ca
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-06-14 18:20:37 +0300

    Добавлено подключение к API и созданы новые компоненты

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppHeader() {
+  return (
+    <div>AppHeader</div>
+  )
+}
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,23 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import React, { useEffect, useState } from "react";
+import AppHeader from "../app-header/app-header";
+
+const url = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
+  const [ingredients, setIngredients] = useState();
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setIngredients(data))
+      .catch(rej => console.log(rej))
+  }, [])
+
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+      <AppHeader />
     </div>
   );
 }
