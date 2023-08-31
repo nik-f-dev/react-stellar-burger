9299f21d0783ae3450a350a44f498b062b974ae3
Nikita Fomichev <nickita.fomichev2016@yandex.ru>
2023-08-31 20:10:30 +0300

    реализованно e2e тестирование

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# Проект: Stellar Burger
+# [Проект: Stellar Burger](https://nik-f-dev.github.io/react-stellar-burger/ "Ссылка на сайт")
 
 Проект Stellar Burger - интернет-магазин с конструктором бургеров.
-На данный момент реализованны: верстка, функционал модальных окон, работа с сервером, перетаскивание ингредиентов и их сортировка.
-В дальнейшем будeт использоваться TypeScript.
 
-Используются технологии html, css, js, react, redux, react-dnd, react-intersection-observer, redux-thunk.
+Используются технологии html, css, ts, react, redux, react-dnd, react-intersection-observer, redux-thunk, react-router-dom.
+Инструменты тестирования jest, cypress.
+
+## Установка и запуск приложения:
+
+- Установка `npm i`
+- Тестирование модулей `npm test`
+- E2E тестирование `npm run test:cy`
+- Запуск приложения `npm start`
---
+++ cypress/e2e/constructor/app.cy.js
@@ -0,0 +1,61 @@
+
+describe('service is available', function() {
+  before(function() {
+    cy.visit('http://localhost:3000');
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/');
+    })
+    cy.intercept('GET', '/api/ingredients', {fixture: 'ingredients.json'});
+  });
+
+  it('should be available on localhost:3000', function() {
+    cy.get('[class^=burger-ingredient_element]').contains('Краторная булка N-200i').should('exist').as('bunElement');
+    cy.get('p[class^=burger-constructor_fieldPrice]').should('exist').contains('0').as('totalPrice');
+    cy.get('[class^=burger-constructor_section]').should('exist').as('dropContainer');
+
+    cy.get('@bunElement').click();
+    cy.get('[class^=modal_closeButton]').should('exist').as('exitButton');
+    cy.get('@exitButton').click();
+
+    cy.get('@bunElement').trigger('dragstart');
+    cy.get('@dropContainer').trigger('drop');
+    cy.get('@totalPrice').contains('2510');
+
+    cy.get('button').contains('Оформить заказ').should('exist').click();
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/login');
+    })
+
+    cy.get('input[type^=email]').should('exist').as('emailInput');
+    cy.get('@emailInput').type('nikita@mail.ru');
+    cy.get('@emailInput').should('have.value', 'nikita@mail.ru');
+
+    cy.get('input[type^=password]').should('exist').as('passwordInput');
+    cy.get('@passwordInput').type('1111{enter}');
+    cy.get('@passwordInput').should('have.value', '1111');
+
+    cy.intercept('POST', '/api/auth/login', {fixture: 'login.json'});
+    cy.intercept('GET', '/api/auth/user', {fixture: 'auth.json'});
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/');
+    })
+
+    cy.get('button').contains('Оформить заказ').should('exist').click();
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/order-modal');
+    })
+
+    cy.get('div[class^=modal_modalBody]').should('exist');
+
+    cy.intercept('POST', '/api/orders', {fixture: 'order.json'});
+
+    cy.get('button[class^=modal_closeButton]').should('exist').click();
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/');
+    })
+  });
+});
